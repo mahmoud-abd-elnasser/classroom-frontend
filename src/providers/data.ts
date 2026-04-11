bc0083592f02aad25f8073f2bbc5234b34284ff4
mahmoud-abd-elnasser <mahmoudabdelnaser248@gmail.com>
2026-04-11 19:13:25 +0200

    finalize create classes form

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,6 +1,6 @@
 import {createDataProvider, CreateDataProviderOptions} from "@refinedev/rest";
 import {BACKEND_BASE_URL} from "@/constants";
-import {ListResponse} from "@/types";
+import {CreateResponse, ListResponse} from "@/types";
 import {HttpError} from "@refinedev/core";
 
 if (!BACKEND_BASE_URL) {
@@ -57,6 +57,14 @@ const options: CreateDataProviderOptions = {
             }
             const payload: ListResponse = await response.json()
             return payload.pagination?.total ?? payload.data?.length ?? 0
+        }
+    },
+    create: {
+        getEndpoint: ({resource}) => resource,
+        buildBodyParams: async ({variables}) => variables,
+        mapResponse: async (response) => {
+            const json: CreateResponse = await response.json()
+            return json.data ?? []
         }
     }
 }
